39ad6cca58467d77b408c108e6d75b2710c60552
Edgar CORTES <edgar.cor.hdz@gmail.com>
2022-11-08 21:41:41 +0100

    Updated files to implement sentry feature
    Updated files:
    - Added sentry_sdk.init at prod_settings.py
    - Added path sentry-debug at urls.py
    - Added sentry-sdk library at requirements.txt

--- oc_lettings_site/prod_settings.py
+++ oc_lettings_site/prod_settings.py
@@ -1,4 +1,6 @@
 from .settings import *
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 DEBUG = False
 
@@ -6,3 +8,19 @@ MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ALLOWED_HOSTS = ['oc-lettings-edaucohe.herokuapp.com']
+
+sentry_sdk.init(
+    dsn="https://49da25c999164788aabe940edfc65cf5@o4504124745711616.ingest.sentry.io/4504124749971456",
+    integrations=[
+        DjangoIntegration(),
+    ],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
